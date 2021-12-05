287e35da1c7f3748297096623f6674d8ef46abbc
849533 <849533@SAIT227692IT.ACDM.DS.SAIT.CA>
2021-12-04 21:45:16 -0700

    ArithmeticCalculator.java -made minor changes to ArithmeticCalculator.java

---
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -0,0 +1,59 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ArithmeticCalculatorServlet extends HttpServlet {
+
+@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // load up a JSP
+        getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
+        return;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // capture the parameters coming in from the POST request
+        String firstnumber = request.getParameter("first_number");
+        String secondnumber = request.getParameter("second_number");
+        
+        // set some attributes in the request object
+        // the request object will be passed through to the JSP by the forward() method
+        request.setAttribute("firstNumber", firstnumber);
+        request.setAttribute("firstNumber", firstnumber);
+        
+        // validations: if the parameters don't exist or are empty, show the first page again
+        if( firstnumber == null || firstnumber.equals("") || secondnumber == null || secondnumber.equals("") || !isNumeric(firstnumber) || !isNumeric(secondnumber) ) {
+            
+            //  set up a helpful error message for the user
+            request.setAttribute("message", "Result: invalid");
+            
+            // display the form again
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request,response);
+            return;
+            
+        }
+        
+        String functionSelected = request.getParameter("function");
+        double solution = 0.0;
+        
+  
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    
+}
